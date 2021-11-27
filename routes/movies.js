726a96287047f921ab16701c4afc2dd543b45662
svr9493929001 <svr9493929001@gmail.com>
2021-11-27 16:31:35 +0530

    done

--- routes/movies.js
+++ routes/movies.js
@@ -9,27 +9,29 @@ import {
 } from "../getMoviesById.js";
 
 router
-.route("/").get(async (request, response) => {
+.route("/")
+.get(async (request, response) => {
   const filtermovies = await showMovies(request);
   response.send(filtermovies);
-}).post(async (request, response) => {
+})
+.post(async (request, response) => {
   const result = await addManyMovies(request);
   response.send(result);
 });
 
-router.get(`/:id`, async (request, response) => {
+router
+.route(`/:id`)
+.get(async (request, response) => {
   const movie = await getMoviesById(request);
   response.send(movie);
-});
-
-router.delete(`/:id`, async (request, response) => {
+})
+.delete(async (request, response) => {
   const movie = await DeleteOne(request);
   const notfound = { message: "No matcing movies" };
   console.log(movie);
   movie ? response.send(movie) : response.status(404).send(notfound);
-});
-
-router.put(`/:id`, async (request, response) => {
+})
+.put(async (request, response) => {
   const movie = await updateOneMovie(request);
   response.send(movie);
 });
